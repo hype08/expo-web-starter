31d2f66a5b17c925345506b7c5531a0468a43657
henry <tarou.88@gmail.com>
2019-11-01 00:38:25 +0900

    offline mode for expo start

--- App.tsx
+++ App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components/native';
+
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient, { gql } from 'apollo-boost';
 import { theme } from './src/utils/theme';
+
+const client = new ApolloClient({ uri: 'http://localhost:4000' });
+
+export const FEED_QUERY = gql`
+  query FeedQuery {
+    feed {
+      id
+      content
+      title
+      published
+    }
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <Text>Hello!</Text>
-      </Container>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Text>Hello</Text>
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
---
+++ src/components/screens/HomeScreen.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+interface Props {}
+
+export const HomeScreen: React.FC<Props> = () => {
+  return <div>Hi</div>;
+};
