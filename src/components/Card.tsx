e37a3416c9e10fd4cd8b314985c49d32a2928a84
henry <tarou.88@gmail.com>
2019-11-01 19:23:40 +0900

    card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled, { ThemeProvider } from 'styled-components/native';
+import styled from 'styled-components/native';
 import { theme } from '../../src/utils/theme';
 
 interface Props {
